798e34922eb502b4aa0831c408a04103f76bd855
ajnel221 <ajnel221@gmail.com>
2021-03-15 13:06:18 +0200

    Initial Push

---
+++ Assets/Scripts/FlightControlls.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightControlls : MonoBehaviour
+{
+    public GameObject cameraPos, thirdPersonCameraPoint;
+    public bool isFirstPerson = false;
+
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+
+    public float lookRotateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance;
+
+    private float rollInput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.P) && isFirstPerson == false)
+        {
+            cameraPos.transform.position = this.transform.position;
+            isFirstPerson = true;
+        }
+
+        if(isFirstPerson == true && Input.GetKey(KeyCode.O))
+        {
+            cameraPos.transform.position = thirdPersonCameraPoint.transform.position;
+            isFirstPerson = false;
+        }
+
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        transform.Rotate(-mouseDistance.y * lookRotateSpeed * Time.deltaTime, mouseDistance.x * lookRotateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.Tab))
+        {
+            forwardSpeed = 5000f;
+        }
+
+        else
+        {
+            forwardSpeed = 25f;
+        }
+
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform[] barrelEnd;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectile, barrelEnd[0].position, barrelEnd[0].rotation) as Rigidbody;
+            projectile.AddForce(barrelEnd[0].forward * 10000*10000);
+        }
+    }
+}
